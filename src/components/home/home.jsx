d0ed62d97a4b929d9375693518343e13ac141659
Fernando <fernando.kaganovicz@gmail.com>
2022-06-15 18:24:40 -0300

    create register and login components

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import homeVideo from '../../media/homeVideo.mp4';
+import NavBar from '../../layouts/navBar';
 import './home.css';
 
 
 const Home = () => {
   return (
     <div className='home-container'>
+      <div className='navBar-space'>
+        <NavBar home='home'/>
+      </div>
       <video className='home-video' src={homeVideo} autoPlay loop muted/>
     </div>
   )
