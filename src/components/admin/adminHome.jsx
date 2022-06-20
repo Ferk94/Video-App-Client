7dd5462a5511dcad28a87508b4a9fa32968e7372
Fernando <Fernando.Kaganovicz@gmail.com>
2022-06-20 19:44:05 -0300

    last push

---
+++ src/components/admin/adminHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminHome = ({ adminInfo }) => {
+  return (
+    <div>
+     Bienvenido {adminInfo.email}
+    </div>
+  )
+}
+
+export default AdminHome;
--- src/router/adminRouter.jsx
+++ src/router/adminRouter.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import AdminHome from '../components/admin/adminHome';
 
 
 const AdminRouter = () => {
+
+  const adminInfo = useSelector(state => state.userInfo);
+
   return (
     <div>
-        hola soy admin router
+        <Routes>
+          <Route path='/*' element={<AdminHome adminInfo={adminInfo}/>}/>
+        </Routes>
     </div>
   )
 }
